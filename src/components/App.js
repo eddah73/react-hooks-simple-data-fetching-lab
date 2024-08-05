c84968dce890657b17e96b5e043bd306a477227a
eddah73 <eddahsjr@gmail.com>
2024-08-06 00:12:15 +0300

    fetching(useEffect)

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [image, setImage] = useState();
+
+    // Fetching image
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => setImage(data.message)); // message is the property that is being returned
+    }, []);
+
+    // Conditional rendering to show loading message
+    if (!image) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default App;
